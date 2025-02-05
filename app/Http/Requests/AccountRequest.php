e83b97f1dbc4766ead9fa01d83473e6ec6ab5ea2
Vasiliy Duvanov <duvanov@ivit.pro>
2025-02-05 23:26:45 +0400

    Update authorization logic in AccountRequest
    Refined the `authorize` method to check permissions dynamically based on user capabilities. This ensures users can only create or update accounts if explicitly allowed. Added `Account` model import to support this change.

--- app/Http/Requests/AccountRequest.php
+++ app/Http/Requests/AccountRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Account;
 use App\Rules\ValidCurrencyRule;
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -18,6 +19,6 @@ class AccountRequest extends FormRequest
 
     public function authorize(): bool
     {
-        return true;
+        return $this->user()->can('create', Account::class)->or($this->user()->can('update', $this->route('account')));
     }
 }
